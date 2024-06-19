91abea6bde0bc328d02dd23bf417e1239b3fc3ae
s1280005 <s1280005@u-aizu.ac.jp>
2024-06-19 21:36:09 +0900

    Station 13 to 17 were finished

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,7 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  let checkbox1 = document.getElementById("check");
+  if (checkbox1.checked) {
+    document.getElementById("text").style.backgroundColor = "red";
+  } else document.getElementById("text").style.backgroundColor = "white";
 }
---
+++ src/scripts/station14.js
@@ -0,0 +1,5 @@
+function butotnClick1() {
+  alert("ECMAScript");
+}
+let button1 = document.getElementById("button1");
+button1.addEventListener("click", butotnClick1);
